70767ce2fa2977342961706650f706b597b9ac8b
Sinantosun <sinan_tosun4141@hotmail.com>
2026-02-18 19:01:23 +0300

    --

--- ImageClassification.ML.Trainer/Models/ModelOutput.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ImageClassification.ML.Trainer.Models
-{
-    public class ModelOutput
-    {
-        public string Prediction { get; set; } // Tahmin edilen kategori adı
-        public float[] Score { get; set; } // Olasılık değerleri
-    }
-}
